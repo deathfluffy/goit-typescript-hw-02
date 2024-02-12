9355c18ed16671475460be35d5c6be0a38ab6375
deathfluffy <mrandreu454@gmail.com>
2024-02-12 17:04:48 +0200

    ++++

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -9,11 +9,9 @@ type AllType = {
   position: number;
   color: string;
   weight: number;
-}
+};
 
-type AllTypeInfo = Pick<AllType, 'name' | 'position' | 'color' | 'weight'>;
-
-function compare(top: AllTypeInfo, bottom: AllTypeInfo): AllType {
+function compare<T extends Pick<AllType, "name" | "color">,U extends Pick<AllType, "position" | "weight">>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
